27ccf702c6dd6a62aaf3d5ca1fb4a2d70281d32b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-04-03 19:45:32 +0100

    Prompt and confirms added with if else statements

--- week3/script.js
+++ week3/script.js
@@ -1,3 +1,66 @@
+// Upper Array
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Lower Array - convert upper array to lower case
+
+// Function to convert text into lower case
+
+var toLower = function (a) {
+return a.toLowerCase();
+}
+
+// Variable contains function run against upper case array to create lower case array. Map method runs the toLower function against each value in the UpperArr array to create a new lower case array
+
+var lowerArr = upperArr.map(toLower);
+
+// Number Array
+
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Symbol Array
+
+var symbolArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+
+// Function to randomly select an index from upper array
+// Function to randomly select an index from lower array
+// Function to randomly select an index from number array
+// Function to randomly select an index from symbol array
+
+// prompt for password length (string) - code should validate user input - loop if input is not between 8 and 128
+// confirm for lower (boolean)
+// confirm for upper (boolean)
+// confirm for number (boolean)
+// confirm for symbol (boolean)
+// code needs to validate that at least one of the above criteria is selected
+
+// Start of generate password function
+
+function generatePassword() {
+  var lengthResponse = parseInt(prompt("How long would you like your password to be (must be between 8 and 128 characters"));
+  if (!lengthResponse) {
+    alert("You need to enter a number between 8 and 128")
+  }
+  
+
+  else if (lengthResponse < 8) {
+    alert("Your selected number is too low. Anyone could crack that password!")
+  }
+  
+  else if (lengthResponse > 128) {
+    alert("More than 128 characters, seriously?! You're never going to remember that. Try again pal.")
+  }
+
+  else {
+    var confirmUpper = confirm("Do you want your password to contain uppercase letters?");
+    var confirmLower = confirm("Do you want your password to contain lowercase letters?");
+    var confirmNumber = confirm("Do you want your password to contain numbers?");
+    var confirmSymbol = confirm("Do you want your password to contain symbols?");
+  }
+}
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,27 +72,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Upper Array
-var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// Lower Array
-var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Number Array
-var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// Symbol Array
-
-
-// Function to randomly select an index from upper array
-// Function to randomly select an index from lower array
-// Function to randomly select an index from number array
-// Function to randomly select an index from symbol array
-
-// prompt for password length (string)
-// prompt for lower (boolean)
-// prompt for upper (boolean)
-// prompt for number length
-// prompt for symbol length
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
