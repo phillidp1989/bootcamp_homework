8b0dc5b0645d3ac34f1eede6078f214fdb69993e
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-04-09 20:16:18 +0100

    Creation of files and basic HTML

---
+++ week4/questions.js
@@ -0,0 +1,26 @@
+var questions = [{
+    title: "How we can declare a function in Javascript",
+    choices: ["function myFunction(...)", "myFunction function(...)", "declare function myFunction(...)", "var function myFunction(...)"],
+    answer: "function myFunction(...)"
+},
+{
+    title: "Which built-in method adds one or more elements to the end of an array?",
+    choices: [".last", ".put", ".push", ".push"],
+    answer: "push( )"
+},
+{
+    title: "How we can call myFunction function in javascript?",
+    choices: ["call function myFunction(...)", "funcall myFunction(...)", "myFunction(...)", "call myFunction(...)"],
+    answer: "myFunction(...)"
+},
+{
+    title: "How do you write 'Hello World' in an alert box?",
+    choices: ["msg('Hello World');", "alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');"],
+    answer: "alert('Hello World');"
+},
+{
+    title: "How to write an IF statement in JavaScript?",
+    choices: ["if i = 5 then", "if i == 5 then", "if i = 5", "if (i == 5)"],
+    answer: "if (i == 5)"
+}
+]
