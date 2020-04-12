fad95676cffe74a193a7ef83e74b726c98f01823
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-04-12 17:19:34 +0100

    Final touches

--- week4/questions.js
+++ week4/questions.js
@@ -25,10 +25,12 @@ var highscores = JSON.parse(localStorage.getItem("scores")) || [];
 
 startButtonEl.addEventListener("click", startQuiz)
 
-// Setting the questions in an array as objects consisting of key value pairs title, choices and answer
+// Variables for the messages presented to the user once an answer is clicked
 
 var correctMessage = "You are correct!"
 var incorrectMessage = "You got that one wrong!"
+
+// Setting the questions in an array as objects consisting of key value pairs title, choices and answer
 
 var questions = [{
 
@@ -77,13 +79,12 @@ var questions = [{
 // Additional variables
 
 var score = 0;
-// var currentQuestion = 0;
 var lastQuestion = questions.length - 1;
 var timeLeft = 0;
 var timer;
 var shuffledQuestions, currentQuestionIndex;
 
-// Function to begin the countdown of the time when the start button is clicked
+// Function to begin the countdown of the time when the start button is clicked. This function starts the timer, hides the 'start' quiz controls and replaces them with the question and answers for question 1
 
 function startQuiz() {
     timeLeft = questions.length * 10;
@@ -91,6 +92,7 @@ function startQuiz() {
     controlsEl.classList.add("hide");
     questionContainerEl.classList.remove("hide");
 
+    // Set interval function to reduce by one each second and for the interval to be cleared when 0 is reached
 
     timer = setInterval(function () {
         timeLeft--;
@@ -101,12 +103,16 @@ function startQuiz() {
         }
     }, 1000);
 
+    // Value assigned to previously unassigned global variable to randomise the production of the questions
+
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0;
     setNextQuestion();
 
 
 }
+
+// Function to set the next question which includes resetting the state of the last question and showing the next question
 
 function setNextQuestion() {
     resetState();
@@ -129,11 +135,15 @@ function showQuestion(question) {
     })
 }
 
+// This function removes the previous questions and answers once they have been selected as the next question gets appended to the div
+
 function resetState() {
     while (answerButtonsEl.firstChild) {
         answerButtonsEl.removeChild(answerButtonsEl.firstChild)
     }
 }
+
+// This function is to determine whether the answer selected by the user is correct based on the event.target and the data attribute .correct. If the answer is correct, a correctClass is added to the button, which changes the styling and the same is true for incorrect answers.
 
 function selectAnswer(e) {
     var selectedButton = event.target;
@@ -152,7 +162,9 @@ function selectAnswer(e) {
         selectedButton.classList.add("incorrectClass");
         timeLeft = timeLeft - 5;
     }
-    // checkAnswer();
+
+    // This function sets a 1 second time between questions to allow the user to see whether their last answer was correct
+
     var betweenQuestionTimer;
     var timeBetweenQuestions = 1;
     betweenQuestionTimer = setInterval(function () {
@@ -168,7 +180,7 @@ function selectAnswer(e) {
         }
     }, 1000);
 
-
+    // Function to clear interval (stop timer) if all questions have been answered
 
 }
 
@@ -177,9 +189,11 @@ function completedQuestions() {
         clearInterval(timer);
         endGame();
         // var earlyComplete = timeLeft;
-       
+
     }
 }
+
+// Function to be called at the end of the game which replaces the last question with the user's score
 
 function endGame() {
     clearInterval(timer);
@@ -193,6 +207,8 @@ function endGame() {
 
 }
 
+// Modal functions obtained from W3Schools to hold high scores
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -204,37 +220,46 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 
+// Counter to prevent user scores being appended multiple times if 'Highscore' button is clicked more than once
+
 var preventDuplicate = 0;
-btn.onclick = function() {
 
-  preventDuplicate++;
-  modal.style.display = "block";
-  if (preventDuplicate <= 1){
-    printHighScore();
-  }
+// Function to display modal and to prevent multiple appends of the same data if clicked multiple times before page refresh
 
-   
+btn.onclick = function () {
+
+    preventDuplicate++;
+    console.log(preventDuplicate);
+    window.counter = preventDuplicate
+    modal.style.display = "block";
+    if (preventDuplicate <= 1) {
+        printHighScore();
+    }
+
+
 }
 
+
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  
-    // while (leaderboardEl.firstChild) {
-    //     leaderboardEl.removeChild(leaderboardEl.firstChild);
-    // }
+window.onclick = function (event) {
 
     if (event.target == modal) {
-    modal.style.display = "none";
-  }
-  
+        modal.style.display = "none";
+    }
+
 }
 
+// Event listener for the Set Score button
+
 setScoreEl.addEventListener("click", setHighScore);
+
+// Function to define user object to be stored to local storage and to push each new object to empty array through a JSON.stringify function to allow this to be stored
 
 function setHighScore() {
     var userObj = {
@@ -242,14 +267,17 @@ function setHighScore() {
         highScore: parseInt(window.totalScore)
     }
 
-    highscores.push(userObj);   
-    localStorage.setItem("scores", JSON.stringify(highscores));  
+    highscores.push(userObj);
+    localStorage.setItem("scores", JSON.stringify(highscores));
+
+    // Once clicked, the printHighScore function is called which appends the user name array to the modal which is then unhidden
+
     printHighScore();
     modal.style.display = "block";
-    
 
 }
 
+// Function to loop through the array containing user objects and append the information (name and score) to the leaderboard modal
 
 function printHighScore() {
 
@@ -258,34 +286,38 @@ function printHighScore() {
     // var sorted = highscores.sort(function(a, b) {return b.score-a.score});
 
     for (var i = 0; i < highscores.length; i++) {
-      console.log(highscores[i].score);
-      var home = document.createElement("li"); //creates new p
-      var words = document.createTextNode(highscores[i].initials + ": " + highscores[i].highScore)  ; //content of p
-      home.appendChild(words);
-      leaderboardEl.appendChild(home);
+        console.log(highscores[i].score);
+        var home = document.createElement("li"); //creates new p
+        var words = document.createTextNode(highscores[i].initials + ": " + highscores[i].highScore); //content of p
+        home.appendChild(words);
+        leaderboardEl.appendChild(home);
     }
 }
 
 // Sort the scores
 
 function scoresSorted(array, key) {
-    return array.sort(function(a,b) {
-      if (a.highScore < b.highScore) {
-        return 1;
-      }
-      return -1;
+    return array.sort(function (a, b) {
+        if (a.highScore < b.highScore) {
+            return 1;
+        }
+        return -1;
     });
-  }
+}
 
+// Function to reload page to allow user to play again
 
-
-playAgain1El.addEventListener("click", function() {
+playAgain1El.addEventListener("click", function () {
     window.location.reload()
 });
 
-playAgain2El.addEventListener("click", function() {
+// Tried to use querySelectorAll to target the playAgain class, but this did not Worker, so added two event listeners
+
+playAgain2El.addEventListener("click", function () {
     window.location.reload()
 });
+
+// Event listener and function to clear local storage and remove users from leaderboard
 
 clearScoresEl.addEventListener("click", function () {
     localStorage.removeItem("scores");
