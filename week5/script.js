153c613a854dca6579206412bc0c3c6d7d232ab5
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-04-18 23:36:00 +0100

    Removing redundant code and tidying up indentations

--- week5/script.js
+++ week5/script.js
@@ -9,48 +9,7 @@
 // BONUS - create button next to each event to move it up and/or down the list of time slots
 // BONUS - when event is input, this creates an element which can then be removed by clicking an x or can be dragged and dropped into another timeslot
 
-
-
-// Create a function to refresh the page at the turn of the hour to keep the planner up to date by showing which events are in the past and which are in the future
-
-// Declaring variables
-
-var currentDate = (moment().format("LL"));
-var currentTime = (moment().format("hh:mm:ssA"))
-
-// Using colon as a delimiter, this will allow me to separate the hour from the current time object and use this to compare against each time block
-
-var hourMinuteSecondSplit = currentTime.split(":");
-console.log(hourMinuteSecondSplit);
-
-
-
-
-$(document).ready(function() {
-
-      
-    // var CurrentTime = moment().format("h:mm:ss")
-    // var secMinHourSplit = CurrentTime.split(":");
-    // var secondsToRefresh = 60 - parseInt(secMinHourSplit[2]);
-    // var minutesToRefresh = 59 - parseInt(secMinHourSplit[1]); 
-    // var totalTimetoRefresh = secondsToRefresh + (minutesToRefresh*60); 
-    // var secondsElapsedSinceRefresh = 0;
-    
-    // var refreshInterval = setInterval(function() {
-    //     secondsElapsedSinceRefresh++
-    //     if(secondsElapsedSinceRefresh === totalTimetoRefresh) {
-    //         var refreshYesNo = confirm("Would you like to refresh your day planner? Any unsaved changes will be lost.")
-
-    //         if(refreshYesNo) {
-    //             window.location.reload();
-    //         } else {
-    //             alert("Your planner has not been automatically updated. Please refresh your page manually ")
-                
-    //             clearInterval(refreshInterval);
-    //         }
-
-    //     }
-    // },1000);
+$(document).ready(function() {       
 
     
 
@@ -71,27 +30,23 @@ $(document).ready(function() {
     CurrentDateTime();
     setInterval(CurrentDateTime, 1000);
 
-    // var globalHour = CurrentDateTime();
-    
-    
-
-
+     
     // Function to update colour of the time block depending on whether it is in the past, present or future
 
     function updateCurrentColour() {
         var currentHour = moment().format("HH");    
         
-        // Each() function created to loop through all elements with the 'description' class i.e. all of the text area fields representing an individual timeblock
+    // Each() function created to loop through all elements with the 'description' class i.e. all of the text area fields representing an individual timeblock
 
         $(".description").each(function() {
 
             $(this).removeClass("present past future");
 
-        // Added parseInt function as my 9am timblock was being assigned with the incorrect class
+    // Added parseInt function as my 9am timblock was being assigned with the incorrect class
 
             var descriptionId = parseInt($(this).attr("id"));
 
-        // If else statement to compare the current hour with the ID of each text area field which corresponds to the timeblock it represents
+    // If else statement to compare the current hour with the ID of each text area field which corresponds to the timeblock it represents
 
             if (currentHour == descriptionId) {
                 $(this).addClass("present");
@@ -106,40 +61,12 @@ $(document).ready(function() {
     }
 
     // Function called to update the colours based on the current hour and a setInterval function used to update this every second
+
     updateCurrentColour();
     setInterval(updateCurrentColour, 1000);
 
     
-    // Function to display saved events when the page is (re)loaded
-
-    // function getEvents() {
-        
-    //     $(".description").each(function () {
-            
-    //         var hourId = $(this).parent().attr("id");
-            
-    //         eventDescription = localStorage.getItem(hourId);
-
-    //         $(this).val(eventDescription)
-    //     })
-    // }
-
-    // getEvents();
-
-    // Function to save events input by user to local storage
-
-    
-    // $(".saveBtn").on("click", function() {
-    //     var hourTimeblock = $(this).parent(".time-block").attr("id");
-    //     var timeblockEvent = $(this).siblings(".description").val();
-        
-
-    //     localStorage.setItem(hourTimeblock, timeblockEvent);
-        
-
-    // })    
-
-    // Function to clear all events
+    // Function to clear all events by clearing local storage and setting the text area of each timeblock to an empty string.
     
     $("#clearEventsBtn").on("click", function() {
         var clearAllQ = confirm("Are you sure you wish to delete all saved events?")
@@ -153,6 +80,7 @@ $(document).ready(function() {
 
     })
 
+    // Function to set the data from local storage to the relevant textarea element upon reloading of the page
 
     function getEvents() {
         var localstorage9 = localStorage.getItem("9am");
@@ -182,10 +110,10 @@ $(document).ready(function() {
         var localstorage17 = localStorage.getItem("5pm");
         $("#17").val(localstorage17);
 
+        
+    }    
 
-    }
-
-    // Calling the getEvents function to ensure that data saved to local storage is displayed upon reloading of the day planner
+    // Calling the getEvents function to ensure that data saved to local storage is displayed upon reloading of the day planner as this sits within the document.ready function.
 
     getEvents();
 
@@ -194,7 +122,7 @@ $(document).ready(function() {
     $("#btn9").on("click", function() {
         var event9 = $("#9").val();
         localStorage.setItem("9am", event9);
-
+        
     })
 
     $("#btn10").on("click", function() {
@@ -244,9 +172,6 @@ $(document).ready(function() {
         localStorage.setItem("5pm", event17);
 
     })
-
-
-    
 
 });
 
